9fbe8e03616b7984ff8ba0e14c3237e755c3614e
Owais Ateeq <owaisatiq.7@gmail.com>
2025-04-06 04:01:38 +0500

    Add files via upload

---
+++ Javascript/Arrays/app.js
@@ -0,0 +1,94 @@
+        //   ARRAYS
+
+//   var cities = ["karachi" , "lahore", "multan"];      
+//   cities[4] = "Islamabad";
+//   cities[0] = "Owais";
+//   console.log(cities,"cities")
+
+
+//   var  arr = ["karachi", "true", "null" , "2012"]
+//   console.log(arr.length)
+
+
+// var cities =  ["karachi", "lahore", "multan"];
+// cities.push("Islamabad", "Owais")
+// cities.pop()
+// cities.pop()
+// console.log(cities)
+
+// cities.unshift("Owais","Ows")
+// cities.shift()
+// console.log(cities)
+
+
+        //    SLICE
+
+        // var cities = ["Karachi","bareli", "Multan", "lahore", "Islamabad"];
+        // var copyArr = cities.slice(0, 3)
+        // var copyArr = cities.slice(1,4)
+        // var copyArr = cities.slice(0, -1)
+        // var copyArr = cities.slice(0)
+        // var copyArr = cities.slice( 1,-3  )
+        // console.log(copyArr)
+        var cities = ["Karachi","bareli", "Multan", "lahore", "Islamabad"];
+ 
+        // //  cities.splice(1,3)
+        // cities.splice( 0, 1  )
+        // cities.splice(1, 0, "hello" )
+        cities.splice(1,1,"islamabad")
+         console.log(cities)       
+ 
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
